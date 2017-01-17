c2968f274ab577fa36f48d5627bba33738b68e93
Paul Hiatt <hiattp@gmail.com>
2017-01-17 13:55:33 +0700

    initial app setup

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'airbrake'
+gem 'rack-timeout'
+gem 'sidekiq'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -38,6 +41,13 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails', '~> 3.5'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'webmock'
+  gem 'database_cleaner'
 end
 
 group :development do
@@ -47,7 +57,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+ruby '2.4.0'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,83 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+if Rails.env.production?
+  abort("The Rails environment is running in production mode!")
+end
+
+require 'rspec/rails'
+require 'webmock/rspec'
+require 'sidekiq/testing'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.run_all_when_everything_filtered = true
+  config.fail_fast = true
+  config.raise_errors_for_deprecations!
+  config.expose_dsl_globally = true
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+    mocks.syntax = :expect
+  end
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  # config.profile_examples = 10
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  # Rails config
+
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # Helpers
+
+  config.include FactoryGirl::Syntax::Methods
+
+  # Hooks
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before do |ex|
+    # Set DatabaseCleaner strategy
+    if ex.metadata[:db_strategy]
+      DatabaseCleaner.strategy = ex.metadata[:db_strategy]
+    else
+      if Capybara.current_driver == :rack_test
+        DatabaseCleaner.strategy = :transaction
+      else
+        DatabaseCleaner.strategy = :truncation
+      end
+    end
+    DatabaseCleaner.start
+    # Ensure Sidekiq testing mode is at default
+    Sidekiq::Testing.fake!
+    # Ensure mailer queue is clear
+    ActionMailer::Base.deliveries.clear
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
